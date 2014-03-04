19c64327210a9e99ce1ca72279c6b7ef783230f6
iplusplusmumbai <subhrajyoti3@gmail.com>
2014-03-04 23:58:38 +0530

    maven complete setup

--- FindAGame/src/main/java/com/ipp/findagame/controller/HomePageController.java
+++ FindAGame/src/main/java/com/ipp/findagame/controller/HomePageController.java
@@ -1,5 +1,46 @@
 package com.ipp.findagame.controller;
 
-public class HomePageController{
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.log4j.Logger;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ipp.findagame.model.Player;
+
+@Controller
+@RequestMapping("/")
+public class HomePageController{
+	
+	static final Logger logger = Logger.getLogger(HomePageController.class);
+	private long i =0 ;
+	/**
+	 * web page response
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/welcome", method = RequestMethod.GET)
+	public String welcome(ModelMap model) {
+		logger.debug("welcome start");
+		model.addAttribute("message", "whatsup!");
+		logger.debug("welcome end");
+		return "index";
+	}
+	
+	/**
+	 * json response 
+	 * 
+	 */
+	@RequestMapping("player")
+    public @ResponseBody Player getDummyPlayer(ModelMap model){
+		Player player = new Player();
+		player.setId(i++);
+		player.setName("Dummy");
+        return player;
+    }
 }
