9872cc3570c1f2ee567493b638ac58add2488689
iplusplusmumbai <subhrajyoti3@gmail.com>
2014-03-07 20:57:53 +0530

    Spring  test

--- FindAGame/src/test/java/com/ipp/findagame/controller/HomePageControllerTest.java
+++ FindAGame/src/test/java/com/ipp/findagame/controller/HomePageControllerTest.java
@@ -1,16 +1,42 @@
 package com.ipp.findagame.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
 
-public class HomePageControllerTest {
+@Test
+@WebAppConfiguration
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml"})
+public class HomePageControllerTest extends AbstractTestNGSpringContextTests{
+	
+	@Autowired 
+	private WebApplicationContext ctx;
+	 
+	private MockMvc mockMvc;
+	
+	@BeforeClass
+	public void beforeClass() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
 
-  @Test
-  public void getDummyPlayer() {
-//    throw new RuntimeException("Test not implemented");
-  }
 
-  @Test
-  public void welcome() {
-//    throw new RuntimeException("Test not implemented");
-  }
+	@Test
+	public void getDummyPlayer() throws Exception {
+		
+	}
+
+	@Test
+	public void welcome() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/welcome","")).
+		andExpect(MockMvcResultMatchers.status().isOk());
+//		throw new RuntimeException("Test not implemented");
+	}
 }
